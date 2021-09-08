ac5e1be85b3a8cf92de52046e84d3a7e5d59528e
Ties <tg.klappe@gmail.com>
2021-09-08 20:26:25 +0200

    Update gitignore, add initial classes for client and server

---
+++ src/spl/chat_application/Client.java
@@ -0,0 +1,10 @@
+package spl.chat_application;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/spl/chat_application/Server.java
@@ -0,0 +1,10 @@
+package spl.chat_application;
+
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
