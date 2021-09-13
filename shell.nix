e2afd7d834c0ad7a9fcc619f882d893cd1c9699f
rowanG077 <goemansrowan@gmail.com>
2021-09-13 15:01:10 +0200

    Fixed featureIDE nix expression.

--- shell.nix
+++ shell.nix
@@ -10,22 +10,35 @@ let
   vscode-with-extensions = pkgs.vscode-with-extensions.override {
     vscodeExtensions = extensions;
   };
-  # featureIDE = stdenv.mkDerivation {
-  #   pname = "FeatureIDE";
-  #   version = "3.8.0";
+  featureIDE = stdenv.mkDerivation {
+    pname = "FeatureIDE";
+    version = "3.8.0";
 
-  #   src = fetchzip {
-  #    url = "https://github.com/FeatureIDE/FeatureIDE/releases/download/v3.8.0/eclipse4.20.0committers-featureide3.8.0-linux64.zip";
-  #    sha256 = "1h1zgzvsjr2y1yh486p54z7zj6igyfpdr09cl039vi1k810jwywn";
-  #    stripRoot = false; 
-  #  };
+    src = fetchzip {
+     url = "https://github.com/FeatureIDE/FeatureIDE/releases/download/v3.8.0/eclipse4.20.0committers-featureide3.8.0-linux64.zip";
+     sha256 = "1h1zgzvsjr2y1yh486p54z7zj6igyfpdr09cl039vi1k810jwywn";
+     stripRoot = false; 
+   };
 
-   # buildInputs = [ pkgs.autoPatchelfHook ];
-  #};
+   installPhase = ''
+     mkdir -p $out/bin
+     cp -r eclipse $out/eclipse
+     makeWrapper $out/eclipse/eclipse $out/bin/eclipse --prefix PATH : ${lib.makeBinPath [ pkgs.jdk ]}
+   '';
+
+   buildInputs = [
+     pkgs.autoPatchelfHook
+     pkgs.gtk3
+     pkgs.cairo
+     pkgs.webkitgtk
+     pkgs.jdk
+     pkgs.makeWrapper
+   ];
+  };
 
 in pkgs.mkShell {
   buildInputs = [
     vscode-with-extensions
-    # featureIDE
+    featureIDE
   ];
 }
